d3b3f13ac63409639bd7a74e03c1824cc2ca183f
Begum Firdousi Abbas <fibeg@amazon.com>
2024-09-20 22:46:45 +0200

    Updated cognito stack, agent instructions, kb response templae

--- deployment/lib/cognito_stack.py
+++ deployment/lib/cognito_stack.py
@@ -18,7 +18,7 @@ class CognitoStack(NestedStack):
     def __init__(self, scope: Construct, construct_id: str, app_name: str, config: Config, application_dns_name: str = None, alb_dns_name : str =None, **kwargs)  -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        unique_string = hashlib.md5(f"{app_name}-{self.region}".encode(), usedforsecurity=False).hexdigest()[:8]
+        unique_string = hashlib.md5(f"{app_name}-{self.region}-{self.account}".encode(), usedforsecurity=False).hexdigest()[:8]
         self.default_user_email=config.default_user_email if hasattr(config, 'default_user_email') else None
         self.default_user_name=config.default_user_name if hasattr(config, 'default_user_name') else None
         self.default_temp_password=config.default_temp_password if hasattr(config, 'default_temp_password') else None
@@ -30,7 +30,7 @@ class CognitoStack(NestedStack):
 
         self.user_pool = cognito.UserPool(
             self,
-            f"{app_name}-user-pool",
+            f"{app_name}-{unique_string}-user-pool",
             account_recovery=cognito.AccountRecovery.EMAIL_ONLY,
             auto_verify=cognito.AutoVerifiedAttrs(email=True),
             self_sign_up_enabled=False,
@@ -43,7 +43,7 @@ class CognitoStack(NestedStack):
         )
 
         self.user_pool_domain = self.user_pool.add_domain(
-            f"{app_name}-user-pool-domain",
+            f"{app_name}-{unique_string}-user-pool-domain",
             cognito_domain=cognito.CognitoDomainOptions(
                 domain_prefix=f"{app_name}-{self.region}-{unique_string}-domain"
             )
